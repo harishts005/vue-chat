f13331a179f1129f394a7ecd4f7abac8befddc79
harishts005 <65065854+harishts005@users.noreply.github.com>
2022-10-05 15:47:55 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  watch: {
+    $route: {
+      handler: (to, from) => {
+        document.title = to.meta.title;
+      },
+      immediate: true,
+    },
+  },
+};
+</script>
+
+<style>
+@import "./assets/styles.css";
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100vw;
+}
+</style>

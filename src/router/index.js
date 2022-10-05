4a80c2b67b3f239e6c2cd81d83566de76aef3414
harishts005 <65065854+harishts005@users.noreply.github.com>
2022-10-05 15:51:47 +0530

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/Login.vue'
+import Signup from '../components/Signup.vue'
+import Chat from '../components/Chat.vue'
+import Profile from '../components/Profile.vue'
+
+
+Vue.use(Router)
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [{
+      path: '/',
+      name: 'Login',
+      component: Login,
+      meta: {
+        auth: true,
+        title: 'Realtime Chat App - Login'
+      }
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup,
+      meta: {
+        auth: true,
+        title: 'Realtime Chat App - Signup'
+      }
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
+      meta: {
+        auth: true,
+        title: 'Realtime Chat App'
+      }
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+      meta: {
+        auth: true,
+        title: 'Realtime Chat App - Profile'
+      }
+    }
+  ],
+
+})
